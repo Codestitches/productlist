3ce7e355610efe80a7f4c936f0b4c7d4822fcfd1
Codestitches <ibibiestitches@gmail.com>
2020-04-25 14:31:08 +0200

    add frontend and requirements.text

--- products/views.py
+++ products/views.py
@@ -5,8 +5,7 @@ from django.shortcuts import render
 from bs4 import BeautifulSoup
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://https://losangeles.craigslist.org/search/?query={}'
-BASE_IMAGE_URL = 'https://'
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/bbb?query={}'
 
 
 def home(request):
@@ -15,37 +14,29 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(quote_plus(search))
     models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    print(final_url)
-    response = requests.get('https://losangeles.craigslist.org/search/?query=python%20tutor&sort=rel')
+    response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
+
     post_listings = soup.find_all('li', {'class': 'result-row'})
+    post_title = post_listings[0].find(class_='result-title').text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find(class_='result-price')
 
     final_postings = []
 
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price')
 
-        if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price').text
-        else:
-            post_price = 'N/A'
+    final_postings.append((post_title, post_url, post_price))
 
-        if post.find(class_='result-image').get('data_ids'):
-            post_image_url = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image_url = BASE_IMAGE_URL.format(post_image_url)
-        else:
-            post_image_url = 'http://craigslist.org/images/peace.jpg'
+    stuff_for_frontend = {
+        'search': search,
+        'final_postings': final_postings,
 
-        final_postings.append((post_title, post_url, post_price, post_image_url))
-
-        stuff_for_frontend = {
-            'search': search,
-            'final_postings': final_postings
-
-        }
-        return render(request, 'my_app/new_search.html', stuff_for_frontend)
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
